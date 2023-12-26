1d6b8d5737709ca9213698e7f4eb87e6630f756c
Victor <strecmit@gmail.com>
2023-12-27 00:09:28 +0200

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите несколько слов:");
+string myString = Console.ReadLine();
+
+string[] mySplitString = myString.Split(" ");
+string temp = "";
+int n = mySplitString.Length - 1;
+for (int i = 0; i < (mySplitString.Length / 2); i++)
+{
+    temp = mySplitString[n];
+    mySplitString[n] = mySplitString[i];
+    mySplitString[i] = temp;
+    n--;
+}
+Console.WriteLine(string.Join(" ", mySplitString));
