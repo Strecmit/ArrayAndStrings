f50e65a46d0fefb3222a46305cf6cc75351d48cd
Victor <strecmit@gmail.com>
2023-12-26 23:45:02 +0200

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите строку:");
+
+string myString = Console.ReadLine();
+int n = myString.Length - 1;
+bool check = false;
+for (int i=0; i < (myString.Length /2); i++){
+    if (myString[i] == myString[n]) {check = true; n--;}
+    else {check = false; break;}
+}
+if (check) {
+    Console.WriteLine("Да, вы ввели палиндром!");
+}
+else {
+    Console.WriteLine("Упс... Ошибочка!");
+}
