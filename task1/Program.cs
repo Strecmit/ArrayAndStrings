53734603304c9b7a1b01af386571b05dee875dd0
Victor <strecmit@gmail.com>
2023-12-26 23:22:17 +0200

    task1 and task2

---
+++ task1/Program.cs
@@ -0,0 +1,17 @@
+﻿char[,] abc = {
+    { '-', 'A', 'B', 'C', 'D', 'E', 'F' },
+    { 'G', 'H', 'I', 'J', 'K', 'L', 'M' },
+    { 'N', 'O', 'P', 'Q', 'R', 'S', 'T' },
+    { 'U', 'V', 'W', 'X', 'Y', 'Z', '-' }
+    };
+string alphabet = "";
+
+for (int i = 0; i < abc.GetLength(0); i++)
+    {
+        for (int j = 0; j < abc.GetLength(1); j++)
+        {
+            alphabet = alphabet + abc[i,j];
+        }
+    }
+
+Console.WriteLine(alphabet);
---
+++ task2/Program.cs
@@ -0,0 +1,27 @@
+﻿//string myString = @"d:\GeekBrain\CharAndArrays\";
+Console.WriteLine("Введите строку, содержащую латинские буквы в верхнем и нижнер регистре:");
+
+string myString = Console.ReadLine();
+string outStr = bigToSmollChar(myString);
+
+Console.WriteLine($"Начальная строка: {myString}");
+Console.WriteLine($"Конечная строка:  {outStr}");
+
+//____________заглавные в строчные
+string bigToSmollChar(string arr)
+{
+    string smollChar = "abcdefghijklmnopqrstuvwxyz";
+    string bigChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    string outArr = "";
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        char ch = arr[i];
+        if (bigChar.IndexOf(ch) >= 0)
+        {
+            outArr = outArr + smollChar[bigChar.IndexOf(ch)];
+        }
+        else { outArr = outArr + arr[i]; }
+    }
+    return outArr;
+}
